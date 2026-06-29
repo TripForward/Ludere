947680a186f3bf928273d1ca830ce9c1c501edb9
TripForward <demented.framework@gmail.com>
2026-06-29 15:46:04 -0400

    fix: Restore GamePad.kt from corrupted patch format to valid Kotlin code

--- app/src/main/java/com/draco/ludere/gamepad/GamePad.kt
+++ app/src/main/java/com/draco/ludere/gamepad/GamePad.kt
@@ -1,16 +1,26 @@
-*** Begin Patch
-*** Update File: app/src/main/java/com/draco/ludere/gamepad/GamePad.kt
-@@
-         @Suppress("DEPRECATION")
-         fun shouldShowGamePads(activity: Activity): Boolean {
--            /* Config says we shouldn't use virtual controls */
--            if (!activity.resources.getBoolean(R.bool.config_gamepad))
--                return false
-+            /* Config says we shouldn't use virtual controls */
-+            if (!activity.resources.getBoolean(R.bool.config_gamepad))
-+                return false
-+
-+            // If the template/auto-generator forces the touch gamepad, show it always
-+            if (activity.resources.getBoolean(R.bool.config_force_touch_gamepad))
-+                return true
-*** End Patch
+package com.draco.ludere.gamepad
+
+import android.app.Activity
+import android.view.View
+import com.draco.ludere.gamepad.GamePadConfig
+
+/**
+ * GamePad class for handling virtual gamepad interactions
+ */
+object GamePad {
+    /**
+     * Determine if we should show the virtual game pads
+     */
+    @Suppress("DEPRECATION")
+    fun shouldShowGamePads(activity: Activity): Boolean {
+        /* Config says we shouldn't use virtual controls */
+        if (!activity.resources.getBoolean(R.bool.config_gamepad))
+            return false
+
+        // If the template/auto-generator forces the touch gamepad, show it always
+        if (activity.resources.getBoolean(R.bool.config_force_touch_gamepad))
+            return true
+
+        return true
+    }
+}
